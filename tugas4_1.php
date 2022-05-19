7d0234fa8d54c49755f7db8278399cea9d9acba1
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:06:25 +0700

    Add files via upload

---
+++ tugas4_1.php
@@ -0,0 +1,15 @@
+<?php
+  $x= 8000;
+  $y = 7500;
+  $z = 300;
+
+  $a = ($x - $y) / $z;
+  $b = ceil($a);
+
+  echo "Harga Beli = $x";
+  echo "</br>Harga Jual = $y";
+  echo "</br>Keuntungan = $z";
+  echo "<br/>Berapa jumlah kambing yang dibeli? Jawaban = $b ekor";
+
+
+ ?>
